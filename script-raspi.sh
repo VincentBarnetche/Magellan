6c65f221a7e6569051b80523ef4664be9a50caaa
VincentBarnetche <vbarnetche@orange.fr>
2018-02-28 13:17:31 +0100

    Update script-raspi.sh

--- script-raspi.sh
+++ script-raspi.sh
@@ -3,36 +3,29 @@
 flight_num=1
 autopilot_name=Pixhawk
 
-#path=/home/pi/$autopilot_name/logs/$(date +%Y-%m-%d)/flight$flight_num
-path_dir=/home/barnetche/Documents/$(date +%Y-%m-%d)
-path=$path_dir/data.txt
+path_dir=/home/pi/$autopilot_name/logs/$(date +%Y-%m-%d)/flight$flight_num
+path_parm=$path_dir/mav.parm
 
-#pour utiliser le bus 1-Wire, ajouter dans le fichier /etc/modules les lignes suivantes :
-#w1-therm
-#w1-gpio pullup=1
-
-#sensor_id=28-0000054c2ec2
-#path_sensor=/sys/bus/w1/devices/$sensor_id/w1_slave
-path_sensor=/home/barnetche/Documents/w1/data.txt
+path_sensor=/sys/bus/w1/devices/
 
 #boucle while à décommenter pour un vol
 #while [ true ]; do
 
+#pour récupérer des paramètres du Pixhawk : afficher fichier mav.parm | sélectionner lignes 2,3 (à modifier selon besoin) | remplace un nombre indéfini d'espaces en tabulation | garde la 2e colonne | remplace les fins de lignes par le caractère '|'
+if [ -e $path_parm ]; then
+cat $path_parm | sed -n '2,3p' | sed 's/  */\t/g' | cut -f2 | tr '\n' '|' >> log.txt
+fi
 
-if [ -e $path ]; then
-cat $path | sed -n '2,3p' | sed 's/  */\t/g' | cut -f2 | tr '\n' '|' >> log.txt
-
+#pour récupérer la température de la sonde DS18B20
 if [ -e $path_sensor ]; then
-cat $path_sensor | sed '/YES/d' | cut -d'=' -f2 >> log.txt
-
-#une fois que la copie est réussie, le répertoire du vol est supprimé pour libérer de l'espace
-if [[ $? -eq 0 ]]; then
-rm -r $path_dir
+find $path_sensor/ -name "28-*" -exec cat {}/w1_slave \; | grep "t=" | awk -F "t=" '{print $2/1000}' >> log.txt #| sed '/YES/d' | cut -d'=' -f2 >> log
 fi
 
-fi
+#une fois que la copie est réussie, le répertoire du vol peut être supprimé pour libérer de l'espace. Attention toutefois à l'incrémentation de $flight_num
+#if [[ $? -eq 0 ]]; then
+#rm -r $path_dir
+#fi
 
-fi
 
 (( flight_num+=1 ))
 
